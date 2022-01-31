0a00361affc022139be9d845e2759ea2278a4dd8
Krb <maurodavidcaceres@gmail.com>
2022-01-31 16:18:13 -0300

    Dark Theme added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,8 @@
 const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
+  mode: "jit",
+  darkMode: "class",
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
