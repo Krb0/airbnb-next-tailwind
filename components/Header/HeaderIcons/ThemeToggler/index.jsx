091113bbb7d7893fc00b999265abb59943b56dcf
Krb <maurodavidcaceres@gmail.com>
2022-01-31 16:18:38 -0300

    Theme toggler added

---
+++ components/Header/HeaderIcons/ThemeToggler/index.jsx
@@ -0,0 +1,34 @@
+import { useState, useEffect } from "react";
+import MoonIcon from "@heroicons/react/solid/MoonIcon";
+import SunIcon from "@heroicons/react/solid/SunIcon";
+const ThemeToggler = ({ theme, setTheme }) => {
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+  const renderThemeChanger = () => {
+    if (!mounted) return null;
+    const currentTheme = theme === "system" ? systemTheme : theme;
+
+    if (currentTheme === "dark") {
+      return (
+        <MoonIcon
+          className="h-4 sm:h-6 cursor-pointer dark:text-gray-200"
+          onClick={() => setTheme("light")}
+        />
+      );
+    } else {
+      return (
+        <SunIcon
+          className="h-4 sm:h-6 cursor-pointer text-yellow-500"
+          onClick={() => setTheme("dark")}
+        />
+      );
+    }
+  };
+
+  return <>{renderThemeChanger()} </>;
+};
+
+export default ThemeToggler;
